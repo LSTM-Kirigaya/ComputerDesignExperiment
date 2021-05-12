9236981404294968040b8628dd8d517e121d5c2d
LSTM-Kirigaya <1193466151@qq.com>
2021-05-12 23:34:01 +0800

    完成部分R类和I类指令测试

--- multistage_pipeline/DataPath/alu_ctrl.v
+++ multistage_pipeline/DataPath/alu_ctrl.v
@@ -39,10 +39,10 @@ module alu_ctrl(funct, ALUOp, alu_ctrl_out);
     parameter AND  = 6'b100100;
     parameter OR   = 6'b100101;
     parameter NOR  = 6'b100111;
+    parameter XOR  = 6'b100110;
 
     parameter SLT  = 6'b101010;
     parameter SLTU = 6'b101011;
-    parameter XOR  = 6'b100110;
     parameter SLL  = 6'b000000;
     parameter SRL  = 6'b000010;
     parameter SRA  = 6'b000011;
@@ -62,11 +62,15 @@ module alu_ctrl(funct, ALUOp, alu_ctrl_out);
             begin
                 case(funct)
                     ADD  : alu_ctrl_out = 4'b0010;
+                    ADDU : alu_ctrl_out = 4'b0010;
                     SUB  : alu_ctrl_out = 4'b0110;
+                    SUBU : alu_ctrl_out = 4'b0110;
                     AND  : alu_ctrl_out = 4'b0000;
                     OR   : alu_ctrl_out = 4'b0001;
-                    SLT  : alu_ctrl_out = 4'b0111;
+                    NOR  : alu_ctrl_out = 4'b1010;
                     XOR  : alu_ctrl_out = 4'b0011;
+                    SLT  : alu_ctrl_out = 4'b0111;
+                    SLTU : alu_ctrl_out = 4'b0111;
                     SLL  : alu_ctrl_out = 4'b1000;
                     SRL  : alu_ctrl_out = 4'b0100;
                     SRA  : alu_ctrl_out = 4'b1100;
